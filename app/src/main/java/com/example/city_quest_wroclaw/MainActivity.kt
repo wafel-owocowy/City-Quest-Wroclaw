c51cc8a9b9446f74c04a9074b59d50786671189f
wafel-owocowy <18188674346+wafel-owocowy@users.noreply.github.com>
2026-04-18 17:20:51 +0200

    nudny ekran startowy

--- app/src/main/java/com/example/city_quest_wroclaw/MainActivity.kt
+++ app/src/main/java/com/example/city_quest_wroclaw/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.city_quest_wroclaw.ui.theme.CityQuestWroclawTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CityQuestWroclawTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    LandingScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun LandingScreen(modifier: Modifier = Modifier){
+    Column(
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CityQuestWroclawTheme {
-        Greeting("Android")
+            .padding(10.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(10.dp, alignment = Alignment.Bottom),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = stringResource(R.string.landing_screen_app_name)
+        )
+        Button(
+            onClick = { TODO() }
+        ) 
+        {
+            Text(
+                text = stringResource(R.string.landing_screen_button_text)
+            )
+        }
     }
 }
