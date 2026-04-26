fff3a16dade779437df76acc51b7e7295f71d104
wafel-owocowy <18188674346+wafel-owocowy@users.noreply.github.com>
2026-04-26 16:56:02 +0200

    dodanie reszty encji do bazy

--- app/src/main/java/com/example/city_quest_wroclaw/RoomDatabase.kt
+++ app/src/main/java/com/example/city_quest_wroclaw/RoomDatabase.kt
@@ -3,15 +3,20 @@ package com.example.city_quest_wroclaw
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.*
-// Klasa reprezentująca encję w bazie
+import java.util.Date
+
+// Klasy reprezentująca encje w bazie
+// lokalizacja
 @Entity(tableName = "locations")
 data class Location(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "audio_guide") val audioGuideId: Int
+    @ColumnInfo(name = "audio_guide") val audioGuideId: Int,
+    @ColumnInfo(name = "lat") val latitude: Float,
+    @ColumnInfo(name = "long") val longitude: Float
 )
-//Klasa będąca interfejsem do encji z możliwymi zapytaniami sql
+// i interfejs do niej
 @Dao
 interface LocationDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -26,12 +31,88 @@ interface LocationDao {
     @Delete
     suspend fun delete(location: Location)
 }
+@Entity(tableName = "history")
+data class Visit(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "timestamp") val time: Date,
+    @ColumnInfo(name = "locationId") val locationId: Int,
+)
+data class VisitWithLocation(
+    @Embedded
+    val location: Location,
+    @Relation(
+        entity = Visit::class,
+        parentColumn = "id",
+        entityColumn = "locationId"
+    )
+    val visitList: List<Visit>
+)
+@Dao
+interface VisitDao{
+    @Transaction
+    @Query("SELECT * FROM history WHERE locationId=:visitId")
+    fun getCategoryWithItsQuotes(visitId: Int): List<VisitWithLocation>
+}
+// dziwactwo na przechowywanie dat
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
+// Encja osiągnięć
+@Entity(tableName = "achievements")
+data class Achievement(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "date") val dateObtained: Date?,
+)
+// i interfejs do niej
+@Dao
+interface AchievementDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(achievement: Achievement)
+
+    @Query("SELECT * FROM achievements")
+    fun getAllAchievements(): LiveData<List<Location>>
+
+    @Query("SELECT * FROM achievements WHERE id = :achievementID LIMIT 1")
+    suspend fun findById(achievementID: Int): Location?
+}
+@Entity(tableName = "stats")
+data class Statistics(
+    @ColumnInfo(name = "username") val username: String,
+    @ColumnInfo(name = "distance") val distance: Float
+)
+@Dao
+interface StatisticsDao{
+    @Query("SELECT username FROM stats")
+    fun getUsername(): String
+
+    @Query("SELECT distance FROM stats")
+    fun getTotalDistance(): Float
+
+    @Query("SELECT COUNT(*) FROM history")
+    fun getVisitCount(): Int
+}
+//Klasa będąca interfejsem do encji z możliwymi zapytaniami sql
+
 // Klasa sklejająca encje i interfejsy w bazę danych
-@Database(entities = [Location::class], version = 1)
+@TypeConverters(Converters::class)
+@Database(entities = [Location::class, Visit::class, Achievement::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun LocationDao(): LocationDao
+    abstract fun VisitDao(): VisitDao
+    abstract fun AchievementDao(): AchievementDao
+    abstract fun StatisticsDao(): StatisticsDao
 }
-// inicjalizator bazy danych
+// inicjalizator bazy danych (chyba powinien bć gdzieś indziej)
 object DatabaseProvider {
     @Volatile
     private var INSTANCE: AppDatabase? = null
